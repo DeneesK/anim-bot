24a6a78b0d70579341d55e094c847315b2d81292
Denis Kalbasov <denis.kalbasov@gmail.com>
2023-11-11 11:23:59 +0300

    app

--- src/app/app.py
+++ src/app/app.py
@@ -1,22 +1,34 @@
 import gradio as gr
 import torch
 
-from diffusers import AutoPipelineForInpainting, UNet2DConditionModel
+
 import diffusers
-from share_btn import community_icon_html, loading_icon_html, share_js
+from diffusers import AutoPipelineForInpainting, UNet2DConditionModel
+from btn import community_icon_html, loading_icon_html, share_js
+
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-pipe = AutoPipelineForInpainting.from_pretrained("diffusers/stable-diffusion-xl-1.0-inpainting-0.1", torch_dtype=torch.float16, variant="fp16").to(device)
+pipe = AutoPipelineForInpainting.from_pretrained(
+    "diffusers/stable-diffusion-xl-1.0-inpainting-0.1",
+    torch_dtype=torch.float16,
+    variant="fp16"
+    ).to(device)
+
 
 def read_content(file_path: str) -> str:
-    """read the content of target file
-    """
+    """read the content of target file"""
     with open(file_path, 'r', encoding='utf-8') as f:
         content = f.read()
 
     return content
 
-def predict(dict, prompt="", negative_prompt="", guidance_scale=7.5, steps=20, strength=1.0, scheduler="EulerDiscreteScheduler"):
+
+def predict(dict, prompt="",
+            negative_prompt="",
+            guidance_scale=7.5,
+            steps=20,
+            strength=1.0,
+            scheduler="EulerDiscreteScheduler"):
     if negative_prompt == "":
         negative_prompt = None
     scheduler_class_name = scheduler.split("-")[0]
@@ -28,13 +40,20 @@ def predict(dict, prompt="", negative_prompt="", guidance_scale=7.5, steps=20, s
         add_kwargs["algorithm_type"] = "sde-dpmsolver++"
 
     scheduler = getattr(diffusers, scheduler_class_name)
-    pipe.scheduler = scheduler.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", subfolder="scheduler", **add_kwargs)
-    
+    pipe.scheduler = scheduler.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0",  # noqa
+                                               subfolder="scheduler",
+                                               **add_kwargs)
+
     init_image = dict["image"].convert("RGB").resize((1024, 1024))
     mask = dict["mask"].convert("RGB").resize((1024, 1024))
-    
-    output = pipe(prompt = prompt, negative_prompt=negative_prompt, image=init_image, mask_image=mask, guidance_scale=guidance_scale, num_inference_steps=int(steps), strength=strength)
-    
+
+    output = pipe(prompt=prompt,
+                  negative_prompt=negative_prompt,
+                  image=init_image,
+                  mask_image=mask,
+                  guidance_scale=guidance_scale,
+                  num_inference_steps=int(steps),
+                  strength=strength)
     return output.images[0], gr.update(visible=True)
 
 
@@ -79,30 +98,29 @@ image_blocks = gr.Blocks(css=css, elem_id="total-container")
 with image_blocks as demo:
     gr.HTML(read_content("header.html"))
     with gr.Row():
-                with gr.Column():
-                    image = gr.Image(source='upload', tool='sketch', elem_id="image_upload", type="pil", label="Upload",height=400)
-                    with gr.Row(elem_id="prompt-container", mobile_collapse=False, equal_height=True):
-                        with gr.Row():
-                            prompt = gr.Textbox(placeholder="Your prompt (what you want in place of what is erased)", show_label=False, elem_id="prompt")
-                            btn = gr.Button("Inpaint!", elem_id="run_button")
-                    
-                    with gr.Accordion(label="Advanced Settings", open=False):
-                        with gr.Row(mobile_collapse=False, equal_height=True):
-                            guidance_scale = gr.Number(value=7.5, minimum=1.0, maximum=20.0, step=0.1, label="guidance_scale")
-                            steps = gr.Number(value=20, minimum=10, maximum=30, step=1, label="steps")
-                            strength = gr.Number(value=0.99, minimum=0.01, maximum=1.0, step=0.01, label="strength")
-                            negative_prompt = gr.Textbox(label="negative_prompt", placeholder="Your negative prompt", info="what you don't want to see in the image")
-                        with gr.Row(mobile_collapse=False, equal_height=True):
-                            schedulers = ["DEISMultistepScheduler", "HeunDiscreteScheduler", "EulerDiscreteScheduler", "DPMSolverMultistepScheduler", "DPMSolverMultistepScheduler-Karras", "DPMSolverMultistepScheduler-Karras-SDE"]
-                            scheduler = gr.Dropdown(label="Schedulers", choices=schedulers, value="EulerDiscreteScheduler")
-                        
-                with gr.Column():
-                    image_out = gr.Image(label="Output", elem_id="output-img", height=400)
-                    with gr.Group(elem_id="share-btn-container", visible=False) as share_btn_container:
-                        community_icon = gr.HTML(community_icon_html)
-                        loading_icon = gr.HTML(loading_icon_html)
-                        share_button = gr.Button("Share to community", elem_id="share-btn",visible=True)
-            
+        with gr.Column():
+            image = gr.Image(source='upload', tool='sketch', elem_id="image_upload", type="pil", label="Upload",height=400)
+            with gr.Row(elem_id="prompt-container", mobile_collapse=False, equal_height=True):
+                with gr.Row():
+                    prompt = gr.Textbox(placeholder="Your prompt (what you want in place of what is erased)", show_label=False, elem_id="prompt")
+                    btn = gr.Button("Inpaint!", elem_id="run_button")
+
+            with gr.Accordion(label="Advanced Settings", open=False):
+                with gr.Row(mobile_collapse=False, equal_height=True):
+                    guidance_scale = gr.Number(value=7.5, minimum=1.0, maximum=20.0, step=0.1, label="guidance_scale")
+                    steps = gr.Number(value=20, minimum=10, maximum=30, step=1, label="steps")
+                    strength = gr.Number(value=0.99, minimum=0.01, maximum=1.0, step=0.01, label="strength")
+                    negative_prompt = gr.Textbox(label="negative_prompt", placeholder="Your negative prompt", info="what you don't want to see in the image")
+                with gr.Row(mobile_collapse=False, equal_height=True):
+                    schedulers = ["DEISMultistepScheduler", "HeunDiscreteScheduler", "EulerDiscreteScheduler", "DPMSolverMultistepScheduler", "DPMSolverMultistepScheduler-Karras", "DPMSolverMultistepScheduler-Karras-SDE"]
+                    scheduler = gr.Dropdown(label="Schedulers", choices=schedulers, value="EulerDiscreteScheduler")
+
+        with gr.Column():
+            image_out = gr.Image(label="Output", elem_id="output-img", height=400)
+            with gr.Group(elem_id="share-btn-container", visible=False) as share_btn_container:
+                community_icon = gr.HTML(community_icon_html)
+                loading_icon = gr.HTML(loading_icon_html)
+                share_button = gr.Button("Share to community", elem_id="share-btn",visible=True)
 
     btn.click(fn=predict, inputs=[image, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[image_out, share_btn_container], api_name='run')
     prompt.submit(fn=predict, inputs=[image, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[image_out, share_btn_container])
