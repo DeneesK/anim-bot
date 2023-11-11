f8508740fc9daa5395806e0b11c8df921649a476
Denis Kalbasov <denis.kalbasov@gmail.com>
2023-11-11 16:15:38 +0300

    fix

--- src/app/app.py
+++ src/app/app.py
@@ -4,7 +4,7 @@ import torch
 
 import diffusers
 from diffusers import AutoPipelineForInpainting, UNet2DConditionModel
-from btn import community_icon_html, loading_icon_html, share_js
+from btn import community_icon_html, loading_icon_html, share_js, css
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -57,43 +57,6 @@ def predict(dict, prompt="",
     return output.images[0], gr.update(visible=True)
 
 
-css = '''
-.gradio-container{max-width: 1100px !important}
-#image_upload{min-height:400px}
-#image_upload [data-testid="image"], #image_upload [data-testid="image"] > div{min-height: 400px}
-#mask_radio .gr-form{background:transparent; border: none}
-#word_mask{margin-top: .75em !important}
-#word_mask textarea:disabled{opacity: 0.3}
-.footer {margin-bottom: 45px;margin-top: 35px;text-align: center;border-bottom: 1px solid #e5e5e5}
-.footer>p {font-size: .8rem; display: inline-block; padding: 0 10px;transform: translateY(10px);background: white}
-.dark .footer {border-color: #303030}
-.dark .footer>p {background: #0b0f19}
-.acknowledgments h4{margin: 1.25em 0 .25em 0;font-weight: bold;font-size: 115%}
-#image_upload .touch-none{display: flex}
-@keyframes spin {
-    from {
-        transform: rotate(0deg);
-    }
-    to {
-        transform: rotate(360deg);
-    }
-}
-#share-btn-container {padding-left: 0.5rem !important; padding-right: 0.5rem !important; background-color: #000000; justify-content: center; align-items: center; border-radius: 9999px !important; max-width: 13rem; margin-left: auto;}
-div#share-btn-container > div {flex-direction: row;background: black;align-items: center}
-#share-btn-container:hover {background-color: #060606}
-#share-btn {all: initial; color: #ffffff;font-weight: 600; cursor:pointer; font-family: 'IBM Plex Sans', sans-serif; margin-left: 0.5rem !important; padding-top: 0.5rem !important; padding-bottom: 0.5rem !important;right:0;}
-#share-btn * {all: unset}
-#share-btn-container div:nth-child(-n+2){width: auto !important;min-height: 0px !important;}
-#share-btn-container .wrap {display: none !important}
-#share-btn-container.hidden {display: none!important}
-#prompt input{width: calc(100% - 160px);border-top-right-radius: 0px;border-bottom-right-radius: 0px;}
-#run_button{position:absolute;margin-top: 11px;right: 0;margin-right: 0.8em;border-bottom-left-radius: 0px;
-    border-top-left-radius: 0px;}
-#prompt-container{margin-top:-18px;}
-#prompt-container .form{border-top-left-radius: 0;border-top-right-radius: 0}
-#image_upload{border-bottom-left-radius: 0px;border-bottom-right-radius: 0px}
-'''
-
 image_blocks = gr.Blocks(css=css, elem_id="total-container")
 with image_blocks as demo:
     gr.HTML(read_content("header.html"))
@@ -109,7 +72,7 @@ with image_blocks as demo:
                         equal_height=True):
                 with gr.Row():
                     prompt = gr.Textbox(
-                        placeholder="Your prompt (what you want in place of what is erased)", 
+                        placeholder="Your prompt (what you want in place of what is erased)",  # noqa
                         show_label=False,
                         elem_id="prompt"
                         )
@@ -118,13 +81,20 @@ with image_blocks as demo:
 
             with gr.Accordion(label="Advanced Settings", open=False):
                 with gr.Row(mobile_collapse=False, equal_height=True):
-                    guidance_scale = gr.Number(value=7.5,
-                                               minimum=1.0,
-                                               maximum=20.0,
-                                               step=0.1,
-                                               label="guidance_scale")
-                    steps = gr.Number(value=20, minimum=10,
-                                      maximum=30, step=1, label="steps")
+                    guidance_scale = gr.Number(
+                        value=7.5,
+                        minimum=1.0,
+                        maximum=20.0,
+                        step=0.1,
+                        label="guidance_scale"
+                    )
+                    steps = gr.Number(
+                        value=20,
+                        minimum=10,
+                        maximum=30,
+                        step=1,
+                        label="steps"
+                    )
                     strength = gr.Number(value=0.99, minimum=0.01,
                                          maximum=1.0, step=0.01,
                                          label="strength")
@@ -192,7 +162,7 @@ with image_blocks as demo:
     gr.HTML(
         """
             <div class="footer">
-                <p>Model by <a href="https://huggingface.co/diffusers" style="text-decoration: underline;" target="_blank">Diffusers</a> - Gradio Demo by 🤗 Hugging Face
+                <p>Model by <a href="https://huggingface.co/diffusers" style="text-decoration: underline;" target="_blank">Diffusers</a> - Hugging Face
                 </p>
             </div>
         """
