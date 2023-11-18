f9bef80d5fd46edda4795501d09a8c586192b881
Denis Kalbasov <denis.kalbasov@gmail.com>
2023-11-18 15:30:25 +0300

    app fix

--- src/app/app.py
+++ src/app/app.py
@@ -3,8 +3,7 @@ from PIL import Image
 
 
 def read_content(file_path: str) -> str:
-    """read the content of target file
-    """
+    """read the content of target file"""
     with open(file_path, 'r', encoding='utf-8') as f:
         content = f.read()
 
@@ -21,60 +20,27 @@ def create_mask(dict):
     return mask
 
 
-css = '''
-.gradio-container{max-width: 1100px !important}
-#image_upload{min-height:400px}
-#image_upload [data-testid="image"], #image_upload [data-testid="image"] > div{min-height: 400px}
-#mask_radio .gr-form{background:transparent; border: none}
-#word_mask{margin-top: .75em !important}
-#word_mask textarea:disabled{opacity: 0.3}
-.footer {margin-bottom: 45px;margin-top: 35px;text-align: center;border-bottom: 1px solid #e5e5e5}
-.footer>p {font-size: .8rem; display: inline-block; padding: 0 10px;transform: translateY(10px);background: white}
-.dark .footer {border-color: #303030}
-.dark .footer>p {background: #0b0f19}
-.acknowledgments h4{margin: 1.25em 0 .25em 0;font-weight: bold;font-size: 115%}
-#image_upload .touch-none{display: flex}
-@keyframes spin {
-    from {
-        transform: rotate(0deg);
-    }
-    to {
-        transform: rotate(360deg);
-    }
-}
-#share-btn-container {padding-left: 0.5rem !important; padding-right: 0.5rem !important; background-color: #000000; justify-content: center; align-items: center; border-radius: 9999px !important; max-width: 13rem; margin-left: auto;}
-div#share-btn-container > div {flex-direction: row;background: black;align-items: center}
-#share-btn-container:hover {background-color: #060606}
-#share-btn {all: initial; color: #ffffff;font-weight: 600; cursor:pointer; font-family: 'IBM Plex Sans', sans-serif; margin-left: 0.5rem !important; padding-top: 0.5rem !important; padding-bottom: 0.5rem !important;right:0;}
-#share-btn * {all: unset}
-#share-btn-container div:nth-child(-n+2){width: auto !important;min-height: 0px !important;}
-#share-btn-container .wrap {display: none !important}
-#share-btn-container.hidden {display: none!important}
-#prompt input{width: calc(100% - 160px);border-top-right-radius: 0px;border-bottom-right-radius: 0px;}
-#run_button{position:absolute;margin-top: 11px;right: 0;margin-right: 0.8em;border-bottom-left-radius: 0px;
-    border-top-left-radius: 0px;}
-#prompt-container{margin-top:-18px;}
-#prompt-container .form{border-top-left-radius: 0;border-top-right-radius: 0}
-#image_upload{border-bottom-left-radius: 0px;border-bottom-right-radius: 0px}
-'''
-
-image_blocks = gr.Blocks(css=css, elem_id="total-container")
+image_blocks = gr.Blocks(css=read_content('style.css'),
+                         title='Naked Bytes',
+                         elem_id="total-container",
+                         analytics_enabled=True)
 with image_blocks as demo:
     gr.HTML(read_content("header.html"))
+    with gr.Row(elem_id="main-container",
+                equal_height=False):
+        with gr.Row(elem_id="image_upload"):
+            image = gr.Image(tool='sketch',
+                             source='upload',
+                             type="pil",
+                             interactive=True,
+                             elem_id="image_up",
+                             container=True)
     with gr.Row():
-        with gr.Row():
-            image = gr.Image(tool='sketch', source='upload', elem_id="image_upload", type="pil", height=400, interactive=True)
-            btn = gr.Button("Отправить!", elem_id="run_button")
+        btn = gr.Button("Раздеть!", elem_id="run_button")
+    with gr.Row():
+        btn_2 = gr.Button("Удалить выбранные элементы!", elem_id="but_2")
+        btn_3 = gr.Button("Загрузить другое фото!", elem_id="but_3")
 
     btn.click(fn=create_mask, inputs=[image], api_name='run')
-
-    gr.HTML(
-        """
-            <div class="footer">
-                <p>
-                </p>
-            </div>
-        """
-    )
 
 image_blocks.queue(max_size=50, concurrency_count=64).launch(share=True)
