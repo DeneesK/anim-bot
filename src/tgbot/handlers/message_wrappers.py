0cf661196834db539be6bc46e6a550374dc1abc8
Denis Kalbasov <denis.kalbasov@gmail.com>
2023-12-05 11:06:16 +0300

    ready

--- src/tgbot/handlers/message_wrappers.py
+++ src/tgbot/handlers/message_wrappers.py
@@ -1,10 +1,14 @@
 from aiogram import types
 
 from src.tgbot.analysis import actions
+from src.settings import const
 
 
 async def call_back_handler(message: types.CallbackQuery):
     if str(message.data).startswith('estimate'):  # noqa
         _, mark = str(message.data).split('-')
         await actions.send_estimate(message, mark)
-        await message.bot.delete_message(message.from_user.id, message.id)
+        await message.bot.delete_message(message.from_user.id,
+                                         message.message.message_id)
+        await message.bot.send_message(message.from_user.id,
+                                       text=const.THE_END)
