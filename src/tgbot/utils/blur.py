163a5f97422e50a8fb0c3ccfe29ca35ba74fa99e
Denis Kalbasov <denis.kalbasov@gmail.com>
2023-12-06 11:13:40 +0300

    ready

---
+++ src/tgbot/utils/blur.py
@@ -0,0 +1,22 @@
+import random
+
+from PIL import Image, ImageFilter
+from download import download
+
+
+async def blur_it(path: str, name: int) -> str:
+    # Open existing image
+
+    path = await download(path, name)
+
+    OriImage = Image.open(path)
+
+    # Applying GaussianBlur filter
+    gaussImage = OriImage.filter(ImageFilter.GaussianBlur(60))
+
+    out_path, _ = path.split('.')
+    out_path = out_path + str(random.randint(0, 500000)) + '.jpg'
+
+    # Save Gaussian Blur Image
+    gaussImage.save(out_path)
+    return out_path
