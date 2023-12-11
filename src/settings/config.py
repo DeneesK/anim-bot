28576ac0ab291ecdd3ed2d42ffdeb4cba069c4ba
Denis Kalbasov <denis.kalbasov@gmail.com>
2023-12-11 09:32:57 +0300

    pre-prod

--- src/settings/config.py
+++ src/settings/config.py
@@ -1,9 +1,10 @@
 from aiogram import Dispatcher
 
-from src.tgbot.handlers.commands import register_commands, register_content_type, register_callback  # noqa
+from src.tgbot.handlers.commands import register_commands, register_content_type, register_callback, register_text  # noqa
 
 
 async def register_all_services(dp: Dispatcher):
     register_commands(dp)
     register_content_type(dp)
     register_callback(dp)
+    register_text(dp)
