6bd190f884587bd1d87218e4cc93e2f87ac12b62
Denis Kalbasov <denis.kalbasov@gmail.com>
2023-12-12 13:35:54 +0300

    pre-prod

--- src/tgbot/utils/download.py
+++ src/tgbot/utils/download.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 async def download(url: str, name: str) -> str:
     async with aiohttp.ClientSession() as session:
-        name = f'imgs/{str(name)+str(random.randint(0, 100))}.jpeg'
+        name = f'img/{str(name)+str(random.randint(0, 100))}.jpeg'
         async with session.get(url) as resp:
             if resp.status == 200:
                 f = await aiofiles.open(
